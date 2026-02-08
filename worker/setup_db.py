c0c14f18e6b45af1690f493bc54ff45a559a2b7b
Appu Cyriac <32483636+falconomad@users.noreply.github.com>
2026-02-08 13:08:48 +0100

    Add files via upload

--- worker/setup_db.py
+++ worker/setup_db.py
@@ -1,7 +1,7 @@
 
 import sqlite3
 
-conn = sqlite3.connect('db/portfolio.db')
+conn = sqlite3.connect('tmp/portfolio.db')
 c = conn.cursor()
 
 c.execute('CREATE TABLE IF NOT EXISTS portfolio (time TEXT, value REAL)')
