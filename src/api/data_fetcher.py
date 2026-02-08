b093e263d10a14ee5b0b388ce297d567dde2de5d
Appu Cyriac <32483636+falconomad@users.noreply.github.com>
2026-02-08 12:12:10 +0100

    Add files via upload

--- src/api/data_fetcher.py
+++ src/api/data_fetcher.py
@@ -1,1 +1,22 @@
 
+import yfinance as yf
+import finnhub
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+FINNHUB_KEY = os.getenv("FINNHUB_API_KEY")
+
+client = finnhub.Client(api_key=FINNHUB_KEY)
+
+def get_price_data(ticker):
+    return yf.download(ticker, period="6mo", interval="1d")
+
+
+def get_company_news(ticker):
+    try:
+        news = client.company_news(ticker, _from="2025-01-01", to="2026-01-01")
+        return [n["headline"] for n in news[:10]]
+    except:
+        return []
