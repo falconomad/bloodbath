62dbd106fc848f1eafe1eebfa654a3c940502f65
Appu Cyriac <appusicloud@icloud.com>
2026-02-10 09:22:42 +0100

    Fix experiment path and Kaibot icon

--- app.py
+++ app.py
@@ -115,8 +115,8 @@ st.markdown(
         position: absolute;
         left: max(calc((100vw - 1200px) / 2 + 1rem), 1rem);
         top: 54%;
-        width: 2.8rem;
-        height: 2.8rem;
+        width: 3.8rem;
+        height: 3.8rem;
         transform: translateY(-50%);
         border-radius: 0.5rem;
         background-image: url('{logo_uri}');
